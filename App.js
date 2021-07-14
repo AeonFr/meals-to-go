8b176c0de0df5e4f249fe4044757366ec65390e5
francisco <francisco@filmin.es>
2021-07-14 19:22:39 +0200

    some initial project setup

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>Our react native blank canvas</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="dark" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { backgroundColor: 'blue', padding: 16 },
+  list: { flex: 1, backgroundColor: 'teal', padding: 16 },
 });
